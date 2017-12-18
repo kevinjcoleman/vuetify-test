3e6a39be3c19483f2c198effeefedbaa9c0a765c
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2017-12-18 14:13:45 -0800

    First commit

---
+++ src/components/data-table.vue
@@ -0,0 +1,24 @@
+<script type="text/javascript">
+  import DataTable from 'vuetify/es5/components/VDataTable'
+
+  /* Extendes Data-table from vuetify
+  * - https://vuetifyjs.com/components/data-tables
+  */
+
+
+  export default {
+    extends: DataTable,
+
+    render (h) {
+      return h('table', {
+        'class': {
+          'datatable table': true,
+          'datatable--select-all': this.selectAll
+        }
+      }, [
+        this.genTHead(),
+        this.genTBody()
+      ])
+    }
+  }
+</script>
